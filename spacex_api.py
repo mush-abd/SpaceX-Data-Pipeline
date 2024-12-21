3731892d4ae6c8ae362dfbe5f5262f714946265d
mush-abd <mushabd.22@gmail.com>
2024-12-21 17:16:55 +0530

    Snowflake prepared

--- spacex_api.py
+++ spacex_api.py
@@ -1,11 +1,18 @@
 import requests
 import pandas as pd
 
-response = requests.get('https://api.spacexdata.com/v4/launches')
-data = response.json()
+# response = requests.get('https://api.spacexdata.com/v4/launches')
+# data = response.json()
 # print(data)
 
-df = pd.DataFrame(data)
+url = 'https://api.spacexdata.com/v4/launches'
+
+df = pd.read_json(url)
+
+print(df.head())
+print(df.dtypes)
+
+# df = pd.DataFrame(data)
 columns = df.columns
 print(columns)
 
